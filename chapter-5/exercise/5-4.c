5e0ade19f635d64c026c276627adc74a12df2499
cyyzero <cyyzero16@qq.com>
2017-11-19 01:09:57 +0800

    exercise 5-2

---
+++ chapter-5/exercise/5-4.c
@@ -0,0 +1,49 @@
+/******************************************
+ * 用fcntl实现dup和dup2
+*******************************************/
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int mydup(int oldfd);
+int mydup2(int oldfd, int newfd);
+int main(int argc, char **argv)
+{
+	int fd, newfd;
+
+	newfd = mydup(0);
+
+	printf("old fd is 0, new fd is %d\n", newfd);
+
+	newfd = mydup2(0, 4);
+
+	printf("old fd is 0, new fd is %d\n", newfd);
+
+	return 0;
+}
+
+int mydup(int oldfd)
+{
+	return fcntl(oldfd, F_DUPFD, 0);
+}
+
+int mydup2(int oldfd, int newfd)
+{
+	if (newfd < 0)
+	{
+		errno = EBADF;
+		return -1;
+	}
+
+	if (fcntl(oldfd, F_GETFL) == -1)
+		return -1;
+
+	if (oldfd == newfd)
+		return old;
+	
+	close(newfd);
+
+	return fcntl(oldfd, F_DUPFD, newfd);
+}
