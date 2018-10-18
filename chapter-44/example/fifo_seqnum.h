40f8a0f84440605ffa0e9e96f12b566902de309b
cyyzero <cyyzero16@qq.com>
2018-10-18 23:53:46 +0800

    Add chapter-44 example

---
+++ chapter-44/example/fifo_seqnum.h
@@ -0,0 +1,24 @@
+#ifndef FIFO_SEQNUM_H
+#define FIFO_SEQNUM_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "tlpi_hdr.h"
+
+#define SERVER_FIFO "/tmp/seqnum_sv"
+#define CLIENT_FIFO_TEMPLATE "/tmp/seqnum_cl.%ld"
+#define CLIENT_FIFO_NAME_LEN (sizeof(CLIENT_FIFO_TEMPLATE) + 20)
+
+struct request
+{
+    pid_t pid;
+    int seq_len;
+};
+
+struct response
+{
+    int seq_num;
+}
+
+#endif
